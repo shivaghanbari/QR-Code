7e21a7637e1a25d602a5868344c52f10b0375b99
Shiva <77195609+shivaghanbari@users.noreply.github.com>
2023-09-09 13:38:33 +0330

    Adding comments to QR.py

--- QR.py
+++ QR.py
@@ -2,15 +2,23 @@ import qrcode
 from PIL import Image, ImageDraw, ImageFont
 
 input_data = "https://mitranail.planovin.com/#/"
+
+# Create a QR code instance
 qr = qrcode.QRCode(
     version=3,
     border=4
 )
+
+# Add data to the QR code
 qr.add_data(input_data)
 qr.make(fit=True)
+
+# Generate a QR code image
 img = qr.make_image(fill_color="white", back_color="black")
 img.save(r"C:\Users\shiva.ghanbari\Desktop\QR_MitraNail.png")
 img = Image.open(r"C:\Users\shiva.ghanbari\Desktop\QR_MitraNail.png")
+
+# Optionally, customize the QR code appearance using PIL
 draw = ImageDraw.Draw(img)
 txt = 'MitraNail'
 font = ImageFont.truetype('arial.ttf', 30)
